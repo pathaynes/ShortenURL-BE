0ca10b645c6b636dbfa6d900a13ad7218c3f8b00
Pat Haynes <pathaynes32@gmail.com>
2019-12-03 10:29:23 -0800

    got some tests to pass

--- test/url.test.js
+++ test/url.test.js
@@ -1,38 +1,72 @@
 require('dotenv').config();
+
 const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
-describe('meme routes', () => {
+
+describe('url routes', () => {
   beforeAll(() => {
-    connect();
+    return connect();
   });
+
   beforeEach(() => {
     return mongoose.connection.dropDatabase();
   });
+  
   afterAll(() => {
     return mongoose.connection.close();
   });
+
   it('creates a custom url', async() => {
     const agent = request.agent(app);
+
     await agent
       .post('/api/v1/auth/signup')
       .send({ username: 'test', password: 'password' });
+
     return agent
-      .post('/api/v1/urls')
-      .send({ 
-        customUrl: 'test-url',
-        longUrl: 'https://www.test.com/'
+      .post('/api/v1/url')
+      .send({
+        customUrl: 'murmuring-chamber',
+        longUrl: 'https://www.google.com/'
       })
       .then(res => {
         expect(res.body).toEqual({
           _id: expect.any(String),
-          customUrl: 'test-url',
-          longUrl: 'https://www.test.com/',
+          customUrl: 'murmuring-chamber',
+          longUrl: 'https://www.google.com/',
+          shortUrl: expect.any(String),
+          hits: 0,
+          user: expect.any(String),
+          __v: 0
+        });
+      });
+
+  });
+
+  it('creates a generated url', async() => {
+    const agent = request.agent(app);
+
+    await agent
+      .post('/api/v1/auth/signup')
+      .send({ username: 'test', password: 'password' });
+
+    return agent
+      .post('/api/v1/url')
+      .send({
+        longUrl: 'https://www.google.com/'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          longUrl: 'https://www.google.com/',
+          shortUrl: expect.any(String),
           hits: 0,
           user: expect.any(String),
           __v: 0
         });
       });
   });
+
 });
